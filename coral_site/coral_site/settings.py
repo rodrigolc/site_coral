8269d5be9955a2058238797888f335247a20e938
rodrigolc <rodrigolpscarvalho@gmail.com>
2020-07-31 21:21:32 -0300

    configurado collectstatic

--- coral_site/coral_site/settings.py
+++ coral_site/coral_site/settings.py
@@ -128,5 +128,6 @@ CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000'
 ]
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
